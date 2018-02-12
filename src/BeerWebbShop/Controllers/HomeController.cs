0418e4d9afdfcd8ded70209694b8c8bf5a10b871
EmelieA <35256773+EmelieA@users.noreply.github.com>
2018-02-12 10:13:02 +0100

    fix the design on the ContactPage, HomePage, and ConfirmOrderPage

---
+++ src/BeerWebbShop/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BeerWebbShop.Models;
+
+namespace BeerWebbShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Beer()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Cart()
+        {
+            ViewData["Message"] = "Your cart";
+
+            return View();
+        }
+
+        public IActionResult ConfirmOrder()
+        {
+            ViewData["Message"] = "confirm order";
+
+            return View();
+        }
+
+
+
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ src/BeerWebbShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BeerWebbShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
